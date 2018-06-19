6ad439c0f95e6a21e1e7c56d9c305622f2f78f5d
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-06-19 09:31:11 +0000

    playing around, nothing special

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/libs', express.static(__dirname + '/node_modules/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+console.log("debug run command:","DEBUG=translite-killer:* npm start");
 
 module.exports = app;
